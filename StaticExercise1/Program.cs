d7d6efd45ecdbf51624b6b0fd2f2530b6a38506c
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-23 15:48:43 -0700

    initial commit

---
+++ StaticExercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelcius(95);
+            var fahrenheit = TempConverter.CelciusTofahrenheit(35);
+
+            Console.WriteLine($"celcius{celcius}");
+            Console.WriteLine($"Fahrenheit{fahrenheit}");
+        }
+        
+        
+    }
+}
---
+++ StaticExercise1/TempConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise1
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+        
+            public static double CelciusTofahrenheit(double celcius)
+
+        { 
+            return (celcius * 9) / 5 + 32;
+        }
+        
+        
+    }
+}
